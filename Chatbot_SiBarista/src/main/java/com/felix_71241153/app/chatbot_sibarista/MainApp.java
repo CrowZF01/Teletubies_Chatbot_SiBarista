2c7bb159f7535863dfa5d6aec68727c34f7f473b
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-04-16 08:36:12 +0700

    Setup Files

---
+++ Chatbot_SiBarista/src/main/java/com/felix_71241153/app/chatbot_sibarista/MainApp.java
@@ -0,0 +1,19 @@
+package com.felix_71241153.app.chatbot_sibarista;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("Chat-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ Chatbot_SiBarista/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.felix_71241153.app.chatbot_sibarista {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.felix_71241153.app.chatbot_sibarista to javafx.fxml;
+    exports com.felix_71241153.app.chatbot_sibarista;
+    exports controller;
+    opens controller to javafx.fxml;
+}
