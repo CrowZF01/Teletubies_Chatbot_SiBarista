e91ff8264f03eb8771906085a50c635540cfb9ec
Devon255 <devonnsp25@gmail.com>
2026-04-16 13:49:33 +0700

    .

--- Chatbot_SiBarista/src/main/java/database/Database.java
+++ Chatbot_SiBarista/src/main/java/database/Database.java
@@ -1,4 +1,23 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private String url = "jdbc:mysql://localhost:3306/chatbot";
+    private String username = "root" ;
+    private String password = "";
+
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public void cekKoneksi() {
+        try(Connection conn = getConnection()) {
+            System.out.println("Terhubung ke Database");
+        }catch (SQLException e ){
+            System.out.println("Koneksi gagal : " + e.getMessage());
+        }
+    }
 }
--- Chatbot_SiBarista/src/main/java/model/Produk.java
+++ Chatbot_SiBarista/src/main/java/model/Produk.java
@@ -1,4 +1,62 @@
 package model;
 
 public class Produk {
+    private String idProduk;
+    private String namaProduk;
+    private String namaKategori;
+    private String deskripsi;
+    private Integer harga;
+    private String statusstok;
+
+    public String getIdProduk() {
+        return idProduk;
+    }
+
+    public void setIdProduk(String idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public String getNamaKategori() {
+        return namaKategori;
+    }
+
+    public void setNamaKategori(String namaKategori) {
+        this.namaKategori = namaKategori;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    public String getStatusstok() {
+        return statusstok;
+    }
+
+    public void setStatusstok(String statusstok) {
+        this.statusstok = statusstok;
+    }
+
+    public String getDetailProduk(){
+        return String.format("%s (%s) - %d [%s]", namaProduk,namaKategori, harga, statusstok);
+    }
 }
